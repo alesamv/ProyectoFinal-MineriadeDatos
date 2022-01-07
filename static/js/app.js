0dcd08aa3e50dec2ca38107f8701761095f28066
alesamv <alesamv@gmail.com>
2022-01-07 09:56:16 -0600

    Proyecto Final

---
+++ static/js/app.js
@@ -0,0 +1,13 @@
+
+const claseTabla = document.getElementsByTagName("table");
+for(var i = 0; i < claseTabla.length; i++)
+    claseTabla[i].classList.add("table");
+   
+
+// const claseRow = document.getElementsByTagName("tr");
+// for(var i = 0; i < claseTabla.length; i++)
+//     claseRow[i].classList.add("table-primary");
+
+// const claseCell = document.getElementsByTagName("td");
+// for(var i = 0; i < claseCell.length; i++)
+//     claseCell[i].classList.add("table-primary");
